9ac506aea198a2b3c69e3a440a4d9352169bfaa9
= <sebbekarlsson97@gmail.com>
2015-05-20 21:52:13 +0200

    more html

--- public/index.php
+++ public/index.php
@@ -1,9 +1,16 @@
 <DOCTYPE html>
 <html>
 	<head>
+		<link rel="stylesheet" type="text/css" href="styles/style.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+		<meta charset="utf-8">
 	</head>
 
 	<body>
+		<h1>This is a h1</h1>
+		<h2>This is a h2</h2>
+		<h3>This is a h3</h3>
+		<p>This is a paragraph</p>
 	</body>
 
 	<footer>
