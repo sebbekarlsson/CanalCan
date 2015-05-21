9d97541138db6c2f8ac2a231b2af0be8d1e3f6e9
= <sebbekarlsson97@gmail.com>
2015-05-21 23:13:02 +0200

    improved style

--- public/includes/docs/video-flow.php
+++ public/includes/docs/video-flow.php
@@ -8,6 +8,15 @@
 	<?php for($i = 0; $i < 1000; $i++){ ?>
 	<div class="video-item">
 		<img src="images/brun.jpg">
+		<div class="text">
+			<strong>Funny Prank Car</strong>
+			<p>So, we decided to do another prank, this one is pretty cool. We hope you...</p>
+		</div>
+		<div class="video-data">
+			<ul>
+				<li>273 0023 views</li>
+			</ul>
+		</div>
 	</div>
 	<?php } ?>
 </div>
