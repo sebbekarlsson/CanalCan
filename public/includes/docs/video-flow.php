835dd63aa1625cde5feb81e6a6ade596833c31d7
= <sebbekarlsson97@gmail.com>
2015-05-21 21:17:18 +0200

    Adds head to content

--- public/includes/docs/video-flow.php
+++ public/includes/docs/video-flow.php
@@ -1,3 +1,9 @@
+<div id="content-head">
+	<div class="text">
+		<h2>The frontpage</h2>
+		<p>Videos with the most upvotes</p>
+	</div>
+</div>
 <div class="video-flow">
 	<?php for($i = 0; $i < 1000; $i++){ ?>
 	<div class="video-item">
